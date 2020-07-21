9415ce0c62fa1415cff2f84053b70fb340929987
Kamal Nasser <kamal@digitalocean.com>
2020-07-22 02:09:06 +0300

    updates

--- nuxt.config.js
+++ nuxt.config.js
@@ -9,7 +9,7 @@ export default {
   ** Nuxt target
   ** See https://nuxtjs.org/api/configuration-target
   */
-  target: 'server',
+  target: 'static',
   /*
   ** Headers of the page
   ** See https://nuxtjs.org/api/configuration-head
--- pages/index.vue
+++ pages/index.vue
@@ -3,7 +3,7 @@
     <div>
       <Logo />
       <h1 class="title">
-        sample-nuxtjs
+        Welcome to Nuxt.js
       </h1>
       <div class="links">
         <a
