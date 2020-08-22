45be0b1613cd26dd0784456fc2d4378d48ada6bb
Steven Normore <snormore@digitalocean.com>
2020-08-22 08:39:56 -0400

    config.resolve.symlinks = false

--- nuxt.config.js
+++ nuxt.config.js
@@ -56,5 +56,9 @@ export default {
   ** See https://nuxtjs.org/api/configuration-build/
   */
   build: {
+    extend (config, ctx) {
+      config.resolve = config.resolve || {}
+      config.resolve.symlinks = false
+    }
   }
 }
