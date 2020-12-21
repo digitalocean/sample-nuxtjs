a8926a218313f30e29bcdab15a359e461a7eae11
shravan-balasubramanian <shravan.balasubramanian@freshworks.com>
2020-12-21 16:39:14 +0530

    Pancha demo

--- pages/index.vue
+++ pages/index.vue
@@ -1,73 +1,95 @@
 <template>
   <div class="container">
-    <div>
-      <Logo />
-      <h1 class="title">
-        Welcome to Nuxt.js
-      </h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
+    <div class="fun-form-btn">
+      <button @click="setRandomStory">
+        Tell a story
+      </button>
+    </div>
+    <div class="some-story" v-if="!isLoading">
+      <div class="title">{{randomTitle.toUpperCase() || 'CLICK'}}</div>
+      <div class="story">{{randomStory || 'Click for a story'}}</div>
+    </div>
+    <div v-else class="load">
+      Loading ....
     </div>
   </div>
 </template>
 
 <script>
-export default {}
+const BASE_URL = 'https://sample-nodejs-4u8rr.ondigitalocean.app/';
+
+export default {
+  name: 'digitalOceanApp',
+  data() {
+    return {
+      allParagraphs: [],
+      randomTitle: '',
+      randomStory: '',
+      isLoading: false,
+    };
+  },
+  methods: {
+    async setRandomStory() {
+      debugger;
+      try {
+        this.isLoading = true;
+        const result = await (await fetch(BASE_URL)).json();
+        this.randomTitle = result.title;
+        this.randomStory = result.paragraphs.join('\n');
+        this.isLoading = false;
+      } catch (er) {
+        this.isLoading = false;
+        alert('Something wrong with the application');
+        console.error(JSON.stringify(er));
+      }
+    },
+  },
+};
 </script>
 
 <style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
+ .container .fun-form-btn {
+   margin-top: 20px;
+   display: flex;
+   justify-content: center;
+ }
+
+.container .fun-form-btn button {
+  background: linear-gradient(#4CB8C4, #3CD3AD);
+  padding: 15px;
+  width: 150px;
+  font-size: 20px;
+  color: white;
+  cursor: pointer;
+  border-radius: 5px;
+
+}
+
+.container .load {
+  margin-top: 25px;
+  font-size: 20px;
+  display:flex;
   justify-content: center;
-  align-items: center;
+}
+
+.container .some-story {
+  margin: 20px;
+  font-size: 20px;
+  border: 2px dotted white;
+  color: white;
+}
+
+.container .title {
+  text-decoration: underline;
+  margin-top: 20px;
   text-align: center;
+  font-size: 25px;
+  font-weight: bold;
 }
 
-.title {
-  font-family:
-    'Quicksand',
-    'Source Sans Pro',
-    -apple-system,
-    BlinkMacSystemFont,
-    'Segoe UI',
-    Roboto,
-    'Helvetica Neue',
-    Arial,
-    sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
+.container .story {
+  margin-top: 10px;
+  font-size: 14px;
+  padding: 15px
 }
 </style>
