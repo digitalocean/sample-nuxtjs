5f55eb56b2f016620d7233b8e71b6da106bde64e
clubvariete <nik@clubvariete.ch>
2022-09-13 13:34:57 +0200

    1

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,8 @@
 <template>
   <div class="container">
     <div>
-      <nuxt-img src="img/v2.png" />
-      <nuxt-img src="assets/v2.png" />
+      <nuxt-img src="/v2.png" />
       <h1 class="title">
-      <nuxt-img src="~/assets/v2.png" />
       Meh Liebi
       </h1>
     </div>
